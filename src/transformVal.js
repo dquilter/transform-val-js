70b065e924dd97bbb7178c94cd5704b1096d6777
dquilter <david_quilter@hotmail.com>
2016-02-29 12:32:30 +0000

    gitignore, scale function, utilities

--- src/transformVal.js
+++ src/transformVal.js
@@ -1,34 +1,65 @@
 (function() {
 	var transformVal = {
 		getTranslate: function(elem, dir) {
-			// Elem takes a jQuery elem
 			// Dir is direction of translation
 			
-//			if (elem instanceof jQuery === false) {
-//				elem = $(elem);
-//			}
+			// Test for jQuery Object and convert
+			transformVal.testjQuery();
 			
 			var val;
 			var matrix = window.getComputedStyle(elem);
 			matrix = matrix.getPropertyValue('transform');
 			
 			// Convert matrix to array
-			matrix = matrix.replace('matrix(', '').replace(')', '');
-			matrix = matrix.split(', ')
-//			console.log(matrix);
+			matrix = transformVal.convertMatrixToArray(matrix);
+
+			yVal = parseInt(matrix[5], 10);
+			xVal = parseInt(matrix[4], 10);
 			
 			if (dir === 'Y') {
-				val = parseInt(matrix[5], 10);
+				val = yVal;
 			}
 			if (dir === 'X') {
-				val = parseInt(matrix[4], 10);
+				val = xVal;
 			}
 			if (dir === undefined) {
-				val = [parseInt(matrix[4], 10), parseInt(matrix[5], 10)];
+				val = [xVal, yVal];
 			}
 			
 			return val;
+		},
+		getScale: function(elem) {
 
+			// Test for jQuery Object and convert
+			transformVal.testjQuery();
+			
+			var val;
+			var matrix = window.getComputedStyle(elem);
+			matrix = matrix.getPropertyValue('transform');
+			
+			// Convert matrix to array
+			matrix = transformVal.convertMatrixToArray(matrix);
+			
+			yVal = parseFloat(matrix[3]);
+			xVal = parseFloat(matrix[0]);
+			
+			val = [xVal, yVal];
+			
+			return val;
+			
+		},
+		
+		// Utility
+		convertMatrixToArray: function(string) {
+			var val = string.replace('matrix(', '').replace(')', '');
+			val = val.split(', ')
+			return val;
+		},
+		testjQuery: function(elem) {
+			if (elem instanceof jQuery) {
+				elem = $(elem)[0];
+			}
+			return elem;			
 		}
 	}
 	
--- tests/tests.js
+++ tests/tests.js
@@ -8,7 +8,6 @@ QUnit.test( "Test Translate Values", function( assert ) {
 	var multiElem = document.createElement('div');
 	multiElem.style.transform = 'translateY(10px) translateX(20px)';
 	
-	
 	elemContainer.appendChild(yElem);
 	elemContainer.appendChild(xElem);
 	elemContainer.appendChild(multiElem);
@@ -16,7 +15,41 @@ QUnit.test( "Test Translate Values", function( assert ) {
 	var xVal = transformVal.getTranslate(xElem, 'X');
 	var multiVal = transformVal.getTranslate(multiElem);
 	
+	var yValJq = document.createElement('div');
+	yValJq.style.transform = 'translateY(75px)';
+	yValJq.classList.add('transformJquery');
+	elemContainer.appendChild(yValJq);
+	var jQueryTest = transformVal.getTranslate($('.transformJquery'), 'Y');
+	
 	assert.equal( yVal, 25, "Got Y Value" );
 	assert.equal( xVal, 40, "Got X Value" );
 	assert.deepEqual( multiVal, [20, 10], "Got Multiple Values" );
+	assert.equal( jQueryTest, 75, "Got Y Value for jQuery Elem" );
+});
+
+QUnit.test( "Test Scale Values", function( assert ) {
+//	var yElem = document.createElement('div');
+//	yElem.style.transform = 'translateY(25px)';
+//	var xElem = document.createElement('div');
+//	xElem.style.transform = 'translateX(40px)';
+	var multiElem = document.createElement('div');
+	multiElem.style.transform = 'scale(1.1, 1.1)';
+	
+//	elemContainer.appendChild(yElem);
+//	elemContainer.appendChild(xElem);
+	elemContainer.appendChild(multiElem);
+//	var yVal = transformVal.getTranslate(yElem, 'Y');
+//	var xVal = transformVal.getTranslate(xElem, 'X');
+	var multiVal = transformVal.getScale(multiElem);
+	
+//	var yValJq = document.createElement('div');
+//	yValJq.style.transform = 'translateY(75px)';
+//	yValJq.classList.add('transformJquery');
+//	elemContainer.appendChild(yValJq);
+//	var jQueryTest = transformVal.getTranslate($('.transformJquery'), 'Y');
+	
+//	assert.equal( yVal, 25, "Got Y Value" );
+//	assert.equal( xVal, 40, "Got X Value" );
+	assert.deepEqual( multiVal, [1.1, 1.1], "Got Multiple Values" );
+//	assert.equal( jQueryTest, 75, "Got Y Value for jQuery Elem" );
 });
